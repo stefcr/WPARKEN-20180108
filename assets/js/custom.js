ee48344e29503357e5c48090cc0e5a2a29f00302
Stefan Cristian <fauxordem@gmail.com>
2018-01-12 04:48:05 +0100

    Version 1.0
    First_Version_20180111

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+// backstretch
+$(document).ready(function () {
+    $("main").backstretch("assets/img/bg-1.jpg").tilt;
+});
+
+$(document).ready(function () {
+    $("section#fifth-page").backstretch("assets/img/bg-3.jpg").tilt;
+});
